ea295f9c7ee168fde6854717ac3e4541c4aa3f8a
Luisde244673 <luisdaviddehoyos2@gmail.com>
2019-07-09 11:09:07 -0500

    Proyecto Java

---
+++ src/rentaautomoviles/Carros.java
@@ -0,0 +1,22 @@
+
+package rentaautomoviles;
+
+public class Carros 
+{
+   public String[] placas = new String[5];
+   public String[] modelos = new String[5];
+   public double[] precios = new double[5];
+   public boolean[] disponible = new boolean[5];
+}
+
+
+
+
+
+
+
+
+
+
+
+
